23c8182f939dfadde7874ce28ca98737bffc9ca7
KD191996 <katyushadanilenko82@gmail.com>
2024-01-04 17:17:56 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -86,3 +86,32 @@ business.addEventListener('click', () => {
     innovative.classList.remove('solution__box-subject-link--active');
     business.classList.add('solution__box-subject-link--active');
 });
+
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     nav: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         }
+//     }
+// })
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
